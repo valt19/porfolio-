e3512eea3bdcb20eade405ce3500f66f4a3ab8fe
valt19 <odeke.trevor@gmail.com>
2023-03-08 08:54:18 +0300

    Add javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+const menubar = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
+
+menubar.addEventListener("click",() => {
+  menubar.classList.toggle("active");
+  navmenu.classList.toggle("active");
+})
